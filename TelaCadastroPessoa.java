0706680367617a5f8321a84474558f7104c1ff7e
JulianoMXM <juliano.massocalegal@gmail.com>
2025-11-23 00:04:43 -0300

    feat: Cria a tela de Cadastro de Pessoa

---
+++ TelaCadastroPessoa.java
@@ -0,0 +1,97 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+public class TelaCadastroPessoa extends JFrame{
+    
+    private Font fonteNegrito = new Font("Arial", Font.BOLD, 24);
+    private Font fontePadrao = new Font("Arial", Font.PLAIN, 24);
+
+    public TelaCadastroPessoa(){
+
+        super("Tela Cadastro Usuário");
+        setSize(500, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
+
+        //Painel Superior
+        JPanel painelTitulo = new JPanel(new FlowLayout());
+        painelTitulo.setBorder(new EmptyBorder(0, 0, 30, 0));
+        JLabel labelTitulo = new JLabel("Cadastro de Usuário");
+        labelTitulo.setFont(fonteNegrito);
+        painelTitulo.add(labelTitulo);
+
+        add(painelTitulo, BorderLayout.NORTH);
+
+        //Painel Central
+        JPanel painelCentral = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        painelCentral.setBorder(new EmptyBorder(20, 0, 20, 0));
+
+        JLabel textoNome = new JLabel("Nome: ");
+        JLabel textoIdade = new JLabel("Idade: ");
+
+        textoNome.setFont(fontePadrao);
+        textoIdade.setFont(fontePadrao);
+
+        textoNome.setHorizontalAlignment(SwingConstants.RIGHT);
+        textoIdade.setHorizontalAlignment(SwingConstants.RIGHT);
+
+        JTextField cxNome = new JTextField(15);
+        JTextField cxIdade = new JTextField(15);
+
+        cxNome.setFont(fontePadrao);
+        cxIdade.setFont(fontePadrao);
+
+        //Configuração GridBagConstraints
+
+        gbc.insets = new Insets(10, 10, 10, 10);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        painelCentral.add(textoNome, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        painelCentral.add(cxNome, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        painelCentral.add(textoIdade, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        painelCentral.add(cxIdade, gbc);
+
+        //-------
+
+        add(painelCentral, BorderLayout.CENTER);
+
+        //Painel Inferior
+        JPanel painelInferior = new JPanel(new GridLayout(2,1, 0,15));
+        painelInferior.setBorder(new EmptyBorder(20, 10, 10, 10));
+
+        JButton botaoCadastro = new JButton("Cadastrar");
+        JButton botaoTelaAdmin = new JButton("Admin");
+        
+        botaoCadastro.setFont(fonteNegrito);
+        botaoTelaAdmin.setFont(fonteNegrito);
+
+        botaoCadastro.addActionListener(event ->{
+            GerenciadorDeSom.tocarClique();
+        });
+
+        botaoTelaAdmin.addActionListener(event ->{
+            GerenciadorDeSom.tocarClique();
+
+        });
+
+        painelInferior.add(botaoCadastro);
+        painelInferior.add(botaoTelaAdmin);
+
+        add(painelInferior, BorderLayout.SOUTH);
+    }
+
+}
