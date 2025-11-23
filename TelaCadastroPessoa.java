93ba893b44918a376957b78a66ca04901dc77900
JulianoMXM <juliano.massocalegal@gmail.com>
2025-11-23 01:59:39 -0300

    fix: Arruma o fechamento de tela

--- TelaCadastroPessoa.java
+++ TelaCadastroPessoa.java
@@ -13,7 +13,7 @@ public class TelaCadastroPessoa extends JFrame{
 
         super("Tela Cadastro Usuário");
         setSize(500, 400);
-        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
 
